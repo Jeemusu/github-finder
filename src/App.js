14b1d6fbd41f01c66d1462e2200325172350754f
James <me@jameswm.co.uk>
2022-11-11 15:06:56 +0900

    added react-router, as well as basic layout and page components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import NotFound from './components/pages/NotFound'
+
 function App() {
     
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <Router>
+            <div className="flex flex-col justify-between h-screen">
+                <Navbar />
+                <main className="container mx-auto px-3 pb-12">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/*" element={<NotFound />} />
+                    </Routes>
+                </main>
+                <Footer />
+            </div>
+        </Router>
     );
 }
 
